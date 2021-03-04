fd4e89d77f38e212b3a96142696234c11b5ae774
21192694 <21192694@DSN1271158611.cfp127aluno.local>
2021-03-04 13:28:57 -0300

    Inicialização da programação

---
+++ PartidaET_com_disp7seg.X/main.c
@@ -0,0 +1,19 @@
+/*
+ * File:   main.c
+ * Author: Gabriel Ruanes Melquiades 
+ *
+ * Created on 4 de Março de 2021, 13:20
+ */
+
+
+#include <xc.h>
+#include "config.h"
+#include "delay.h"
+
+void main(void) 
+{
+    while ( 1 )
+    {
+        
+    }
+}
